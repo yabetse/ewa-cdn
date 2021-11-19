08526ffd7ef4b5a08009f219ac996978f0f25246
MalekTrabelsi <trabmalek7@gmail.com>
2021-11-19 08:37:37 +0700

    request form error msg

--- dist/request.js
+++ dist/request.js
@@ -45,7 +45,7 @@ display_message = function (json_obj) {
     var error_msg = json_obj["error"];
     $(".error-message-custom").hide();
     $(".validation-message-custom").hide();
-    $("<div class='error-message-custom'><strong>" + error_msg + "</strong></div>").insertAfter($("#view_60 form ul"));
+    $("<div class='error-message-custom'><strong>" + error_msg + "</strong></div>").insertBefore($("#view_60 form ul"));
     // setTimeout(hide_error, 5000);
   }
 
