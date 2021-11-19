ce3eee24f5b8cf2deba3877488afad3034957d8d
yabetse <yabetse065@gmail.com>
2021-11-19 21:31:47 +0300

    Optimized payoff and cutoff code

--- dist/dashboard.js
+++ dist/dashboard.js
@@ -1,59 +1,61 @@
 requests_check = function (cutoff_day, nb_requests, max_nb_requests) {
-    // condition1 : cutoff date
-    if (cutoff_day == "-") {
-      var cond1 = false;
-    } else {
-      var cond1 = new Date() <= new Date(cutoff_day.split("/")[2], cutoff_day.split("/")[1] - 1, cutoff_day.split("/")[0]);
-    }
-  
-    // condition2: total number of requests per month
-    var cond2 = max_nb_requests <= 0 || nb_requests < max_nb_requests;
-  
-    // compiling all
-    if (cond1 && cond2) {
-      return true;
-    } else {
+  // condition1 : cutoff date
+  if (cutoff_day == "-") {
+    var cond1 = false;
+  } else {
+    var cond1 = new Date() <= new Date(cutoff_day.split("/")[2], cutoff_day.split("/")[1] - 1, cutoff_day.split("/")[0]);
+  }
+
+  // condition2: total number of requests per month
+  var cond2 = max_nb_requests <= 0 || nb_requests < max_nb_requests;
+
+  // compiling all
+  if (cond1 && cond2) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Payoff and Cutoff Dates
+let current_month = new Date().getFullYear() + "-" + (new Date().getMonth() + 1);
+
+let monthIndex = -1;
+let payday = "-";
+let cutoff_day = "-";
+
+let months = $("#view_68 .kn-detail.field_88 .kn-detail-body span span span");
+let payoffs = $("#view_68 .kn-detail.field_76 .kn-detail-body span span span");
+let cutOffs = $("#view_68 .kn-detail.field_82 .kn-detail-body span span span");
+
+$(months).each(function (index) {
+  if ($(this).text() === current_month) {
+    monthIndex = index;
+
+    return false;
+  }
+});
+
+if (payOffs.length) {
+  $(payoffs).each(function (index) {
+    if (index === monthIndex) {
+      payday = $(this).text();
+
       return false;
     }
-  };
-  
-// Payoff and Cutoff Dates --> code to be optimized
-var current_month = new Date().getFullYear() + "-" + (new Date().getMonth() + 1);
-
-var months = Array();
-$("#view_68 .kn-detail.field_88 .kn-detail-body span span span").each(
-  function () {
-    months.push($(this).text());
-  }
-);
-
-var payoffs = Array();
-$("#view_68 .kn-detail.field_76 .kn-detail-body span span span").each(
-  function () {
-    payoffs.push($(this).text());
-  }
-);
-
-var cutoffs = Array();
-$("#view_68 .kn-detail.field_82 .kn-detail-body span span span").each(
-  function () {
-    cutoffs.push($(this).text());
-  }
-);
-
-for (var i = 0; i < months.length; i++) {
-  if (months[i] == current_month) {
-    var payday = payoffs[i];
-    var cutoff_day = cutoffs[i];
-  }
+  });
 }
 
-if (payoffs.length == 0) {
-  var payday = "-";
-}
+if (cutOffs.length) {
+  $(cutOffs).each(
+    function (index) {
+      if (index === monthIndex) {
+        cutoff_day = $(this).text();
 
-if (cutoffs.length == 0) {
-  var cutoff_day = "-";
+        return false;
+      }
+    }
+  )
 }
 
 // Withdrawable Amount and Other Conditions
@@ -76,22 +78,22 @@ var available_amount = balance - requested_amount;
 
 var check = requests_check(cutoff_day, requested_transactions, max_number_requests);
 
-var html = '<section id="custom-view-scene1">'
-          +   '<div class="payday-wrapper">'
-          +      '<div>'
-          +         '<div class="payday-label">Next Payday</div>'
-          +         '<div class="payday-value">' + payday + '</div>'
-          +         '<span class="cutoff-message"><i>Withdrawals until ' + cutoff_day + '</i></span>'
-          +      '</div>'
-          +      '<img src="https://ewa-services.com/ewa/images/ico-calendar.svg"/>'
-          +   '</div>'
-          +   '<div class="max-withdrawable">'
-          +      '<div class="max-withdrawable-label">Maximum Withdrawable Amount</div>'
-          +      '<div class="max-amount-button">'
-          +         '<span>' + (available_amount * withdrawable_threshold).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '</span>'
-          +         '<a' + (check == true ? ' href="' + window.location.pathname + '#request"' : ' style="pointer-events:none;" class="disabled"') + '>Withdraw</a>'
-          +      '</div>'
-          +   '</div>'
-          +'</section>';
+var html = '<section id="custom-view-scene1">' +
+  '<div class="payday-wrapper">' +
+  '<div>' +
+  '<div class="payday-label">Next Payday</div>' +
+  '<div class="payday-value">' + payday + '</div>' +
+  '<span class="cutoff-message"><i>Withdrawals until ' + cutoff_day + '</i></span>' +
+  '</div>' +
+  '<img src="https://ewa-services.com/ewa/images/ico-calendar.svg"/>' +
+  '</div>' +
+  '<div class="max-withdrawable">' +
+  '<div class="max-withdrawable-label">Maximum Withdrawable Amount</div>' +
+  '<div class="max-amount-button">' +
+  '<span>' + (available_amount * withdrawable_threshold).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + '</span>' +
+  '<a' + (check === true ? ' href="' + window.location.pathname + '#request"' : ' style="pointer-events:none;" class="disabled"') + '>Withdraw</a>' +
+  '</div>' +
+  '</div>' +
+  '</section>';
 
 $(html).insertBefore($("#kn-scene_1"));
